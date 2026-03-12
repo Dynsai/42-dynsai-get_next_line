ebc70997a827a6b52726291766d59b67b73420c1
Dynsai <DynsaiCode@outlook.com>
2026-03-12 18:10:01 +0100

    Reorganizado

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../include/get_next_line.h"
 
 char	*get_next_line(int fd);
 char	*_fill_line_buffer(int fd, char *left_c, char *buffer);
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "../include/get_next_line.h"
 //I can have as many functions i want here
 
 char	*ft_strjoin(char const *s1, char const *s2);
